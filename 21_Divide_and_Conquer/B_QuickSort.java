e76f966140aaf7bb10a3c4fca1c28e060372d094
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-08-16 03:19:43 +0530

    init

---
+++ 21_Divide_and_Conquer/B_QuickSort.java
@@ -0,0 +1,42 @@
+public class B_QuickSort {
+    public static void printArr(int arr[]){
+        for ( int i = 0;i <arr.length ;i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void quickSort(int arr[], int si, int ei){
+        if(si>=ei){
+            return;
+        }
+        //last element
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx-1); //left side
+        quickSort(arr, pIdx+1, ei); //right side
+    }
+
+    public static int partition(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i = si-1; //to make place for element smaller than pivot
+
+        for(int j=si;j<ei;j++){
+            if(arr[j] <= pivot){
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+    public static void main(String args[]){
+        int arr[] = {6, 3, 9, 8, 2, 5};
+        quickSort(arr, 0, arr.length-1);
+        printArr(arr);
+    }
+}
