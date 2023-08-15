8bf1ad3eb4d70f14b28add7620f526cde11e0705
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-08-15 12:07:57 +0530

    init

--- 21_Divide_and_Conquer/A_MergeSort.java
+++ 21_Divide_and_Conquer/A_MergeSort.java
@@ -1,4 +1,10 @@
 public class A_MergeSort {
+    public static void printArr(int arr[]){
+        for ( int i = 0;i <arr.length ;i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void mergesort(int arr[], int si,int ei){
         if (si>=ei){
             return;
@@ -13,9 +19,38 @@ public class A_MergeSort {
     public static void merge(int arr[], int si, int mid, int ei){
         int temp[] = new int[ei-si+1];
         int i = si;
-        int j = 
+        int j = mid + 1;
+        int k = 0;
+
+        while(i<=mid && j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        //left part
+        while(i <= mid){
+            temp[k++] = arr[i++];
+        }
+
+        //right part
+        while(j <= ei){
+            temp[k++]=arr[j++];
+        }
+
+        //copy temp to original array
+        for(k=0,i=si;k<temp.length;k++,i++){
+            arr[i] = temp[k];
+        }
     }
-    public static void main(String args){
+    public static void main(String args[]){
         int arr[] = {6, 3, 9, 5, 2, 8};
+        mergesort(arr, 0, arr.length-1);
+        printArr(arr);
     }
 }
