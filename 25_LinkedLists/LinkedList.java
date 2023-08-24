e43460feb36dc492365f5b6db1e01f7dae49745e
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-08-25 02:35:41 +0530

    init

--- 25_LinkedLists/LinkedList.java
+++ 25_LinkedLists/LinkedList.java
@@ -26,9 +26,45 @@ public class LinkedList {
         //step 3:- head = newNode
         head = newNode;
     }
+
+    public void addLast(int data){
+        //step 1:- Create new node
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        //step 2:- tail.next = newNode
+        tail.next = newNode;
+
+        //step 3:- tail = newNode
+        tail = newNode;
+    }
+
+    public void print(){
+        if(head == null){
+            System.out.print("LL is empty");
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+
+    }
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
+        ll.print();
         ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
     }
 }
