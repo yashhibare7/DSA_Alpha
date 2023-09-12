51f26aa93f6d450b39ee8f2c21e0536314fb6610
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-09-12 19:55:59 +0530

    init

---
+++ 24_ArrayLists/A_Classroom.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class A_Classroom {
+    public static void main(String args[]){
+        ArrayList<Integer> List = new ArrayList<>();
+
+        List.add(2);
+        List.add(3);
+        List.add(4);
+        List.add(5);
+        System.out.println(List);
+
+    }
+}
