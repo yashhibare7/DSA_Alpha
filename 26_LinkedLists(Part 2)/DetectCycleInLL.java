df18105b90cca61015447f4c9848397493b70940
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-09-22 23:26:29 +0530

    init

---
+++ 26_LinkedLists(Part 2)/DetectCycleInLL.java
@@ -0,0 +1,39 @@
+public class DetectCycleInLL{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public static boolean detectloop(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static void main(String args[]){
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = head;
+
+        System.out.println(detectloop());
+        
+    }
+}
