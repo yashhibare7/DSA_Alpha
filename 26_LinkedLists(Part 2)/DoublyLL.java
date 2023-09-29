dbcebff1c54742bb9033d223184edb2c710bb702
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-09-30 02:22:22 +0530

    init

--- 26_LinkedLists(Part 2)/DoublyLL.java
+++ 26_LinkedLists(Part 2)/DoublyLL.java
@@ -1,5 +1,52 @@
 public class DoublyLL {
+    public static class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //add
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    //print
+    public void print(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + " <--> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
     public static void main(String args[]){
-        
+        DoublyLL dll = new DoublyLL();
+        dll.addFirst(5);
+        dll.addFirst(4);
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.print();
+        System.out.println(size);
     }
 }
