b39da70548c0c9c4ec56664e31832d74de0dd150
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-10-03 23:20:50 +0530

    init

--- 26_Stacks/ReverseAStack.java
+++ 26_Stacks/ReverseAStack.java
@@ -1,6 +1,41 @@
 //Question 3
 //Reverse a Stack
 
+import java.util.Stack;
+
 public class ReverseAStack {
-    
+    public static void pushBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushBottom(s, data);
+        s.push(top);
+    }
+
+    public static void revStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        revStack(s);
+        pushBottom(s, top);
+
+    }
+
+    public static void printStack(Stack<Integer> s){
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        revStack(s);
+        printStack(s);
+    }
 }
