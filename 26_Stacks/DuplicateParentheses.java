4a50ec68d50b37dad857bc4467425cfe2829fd4f
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-10-19 04:10:25 +0530

    init

--- 26_Stacks/DuplicateParentheses.java
+++ 26_Stacks/DuplicateParentheses.java
@@ -1,5 +1,34 @@
+import java.util.Stack;
+
 public class DuplicateParentheses {
+    public static boolean isDuplicate(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == ')') {
+                int count = 0;
+                while(s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if(count < 1){
+                    return true;
+                }else{
+                    s.pop();
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+
+        return false;
+    }
     public static void main(String args[]){
-        
+        String str = "((a+b))"; //true
+        String str2 = "(a-b)"; //false
+        System.out.println(isDuplicate(str));
+        System.out.println(isDuplicate(str2));
     }
 }
