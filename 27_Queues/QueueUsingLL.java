075a519fcbe30d7bd926afe1aed05fbef2c18265
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-12-20 20:50:29 +0530

    init

---
+++ 27_Queues/QueueUsingLL.java
@@ -0,0 +1,70 @@
+public class QueueUsingLL {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class Queues {
+        static Node head = null;
+        static Node tail = null;
+
+        public static boolean isEmpty(){
+            return head == null & tail == null;
+        }
+
+        //add
+        public static void add(int data){
+            Node newNode = new Node(data);
+            if(head == null) {
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        //remove
+        public static int remove() {
+            if(isEmpty()) {
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            int front = head.data;
+
+            //Single element
+            if(tail == head){
+                tail = head = null;
+            } else {
+                head= head.next;
+            }
+
+            return front;
+        }
+
+        //peek
+        public static int peek() {
+            if(isEmpty()) {
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String ars[]){
+        Queues q = new Queues();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
