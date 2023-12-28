78aada5c860379390bd6be4d60bd136254f643e7
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-12-28 21:31:30 +0530

    init

---
+++ 13_Arrays/G_MaxSubarraySum.java
@@ -0,0 +1,26 @@
+public class G_MaxSubarraySum {
+    public static void maxSubarraySum(int numbers[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i=0;i<numbers.length;i++){
+            int start=i;
+            for(int j=i;j<numbers.length;j++){
+                int end = j;
+                currSum = 0;
+                for(int k = start; k<=end;k++){
+                    currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                if(maxSum<currSum){
+                maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max Sum =" + maxSum);
+    }
+    public static void main(String[] args) {
+        int numbers[] = { 2,4,6,8,10};
+        maxSubarraySum(numbers);
+    }
+}
