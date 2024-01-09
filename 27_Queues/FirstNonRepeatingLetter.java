969117c915fb00f10b1e1a7ff3bedd05d7114967
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-09 23:58:47 +0530

    init

---
+++ 27_Queues/FirstNonRepeatingLetter.java
@@ -0,0 +1,16 @@
+//First Non-repeating Letter in a stream of characters
+//aabccxb ------> a -1 b b b b x
+// a   --> a
+// aa   --> -1
+// aab  --> b
+// aabc --> b
+// aabcc --> b
+// aabccx --> b
+// aabccxb --> x
+
+
+import java.util.*;
+
+public class FirstNonRepeatingLetter {
+    
+}
