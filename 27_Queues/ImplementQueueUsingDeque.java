e7c021f263244459e62ea20fa09c00736220dd2c
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-11 23:55:01 +0530

    init

--- 27_Queues/ImplementQueueUsingDeque.java
+++ 27_Queues/ImplementQueueUsingDeque.java
@@ -1,7 +1,31 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class ImplementQueueUsingDeque {
+    static class Queue {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void add(int data){
+            deque.addLast(data);
+        }
+
+        public int remove() {
+            return deque.removeFirst();
+        }
+
+        public int peek() {
+            return deque.getFirst();
+        }
+    }
     public static void main(String args[]){
-        
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("peek = "+ q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
     }
 }
