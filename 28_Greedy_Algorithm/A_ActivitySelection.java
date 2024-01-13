3c493deba510fea7c6981ce24a7aed8f2e6e84f0
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-13 23:37:55 +0530

    init

---
+++ 28_Greedy_Algorithm/A_ActivitySelection.java
@@ -0,0 +1,22 @@
+//Activity Selection -->
+
+//You are given n activities with their start and end times. Select the maximum number of activities that can 
+//be performed by a single person, assuming that a person can only work on a single activity at a time. 
+//Activities are sorted according to end time.
+
+//start=[10,12,20]             ans = 2(A0 & A2)                            
+//end = [20,25,30]  
+
+//                                                                         -------------------------
+//                                                                                          --------
+//                                                                         -----------
+//                                           ------------------------------------
+//Approach                                                     -------
+//Start = [1,3,0,5,8,5]                            -------
+//End = [2,4,6,7,9,9]                        -------------------------------------------------------
+                                        //   |     |     |     |     |     |     |     |     |     |
+                                        //   0     1     2     3     4     5     6     7     8     9
+
+public class A_ActivitySelection{
+    
+}
