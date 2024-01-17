7d3fce73c5d4fffacc87a44f202780e9d542a611
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-18 02:27:32 +0530

    Fractional Knapsack

---
+++ 28_Greedy_Algorithm/C_FractionalKnapsack.java
@@ -0,0 +1,13 @@
+//Given the weights and values of N items, put these items in a knapsack of capacity W to get the  maximum  total 
+//value in the kanpsack.
+
+//value = [60,100,120]
+//weight = [10,20,30]          ans = 240
+//W = 50
+
+
+public class C_FractionalKnapsack{
+    public static void main(String args[]){
+
+    }
+}
