6c402041b753807ce5d14f934cbe7d2660415fb0
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-19 19:04:24 +0530

    Job Sequencing Problem Question

---
+++ 28_Greedy_Algorithm/G_JobSequencingProblem.java
@@ -0,0 +1,14 @@
+//Given an array of jobs where every job has a deadline and profit if the job is finished before the deadline. It is 
+//also given that every job takes a single unit of time, so the minimum possible deadline for any job is 1. Maximize
+//the total profit if only one job can be scheduled at a time.
+
+//Job A = 4,20
+//Job B = 1,10            ans=C,A
+//Job C = 1,40
+//Job D = 1,30
+
+
+
+public class G_JobSequencingProblem{
+    public static void main(String args[]){}
+}
