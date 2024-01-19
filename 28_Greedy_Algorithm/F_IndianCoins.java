1c2b250cb4eba4e70ec4f91d07d4d50e9b498783
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-19 18:36:49 +0530

    Indian coins Problem Solution

--- 28_Greedy_Algorithm/F_IndianCoins.java
+++ 28_Greedy_Algorithm/F_IndianCoins.java
@@ -10,5 +10,30 @@
 
 import java.util.*;
 public class F_IndianCoins {
-    public static void main(String args[]){}
+    public static void main(String args[]){
+        Integer coins[] = {1,2,5,10,20,50,100,500,2000};
+
+        Arrays.sort(coins, Comparator.reverseOrder());
+
+        int countOfcoins = 0;
+        int amount = 590;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(int i=0;i<coins.length;i++){
+            if (coins[i] <= amount) {
+                while (coins[i] <= amount) {
+                    countOfcoins++;
+                    ans.add(coins[i]);
+                    amount -= coins[i];
+                }
+            }
+        }
+
+        System.out.println("Total (min) coins used = "+ countOfcoins);
+
+        for(int i=0;i<ans.size();i++){
+            System.out.println(ans.get(i)+" ");
+        }
+        System.out.println();
+    }
 }
