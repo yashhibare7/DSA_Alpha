5f12830621a7848144d1d5ba5fceaf569df7525f
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-19 18:03:11 +0530

    init

---
+++ 28_Greedy_Algorithm/F_IndianCoins.java
@@ -0,0 +1,14 @@
+//We are infinite supply of denominations [1,2,5,10,20,50,100,500,2000].
+//Find min no. of coins/notes to make change for a value V.
+
+//V =121
+//ans = 3(100+20+1)
+
+//V = 590
+//ans = 4(500+50+20+20)
+
+
+import java.util.*;
+public class F_IndianCoins {
+    public static void main(String args[]){}
+}
