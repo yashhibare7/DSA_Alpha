7e11231bb41435a1657b074224e40d590773e249
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-22 16:08:53 +0530

    Chocola Problem

---
+++ 28_Greedy_Algorithm/H_Chocola.java
@@ -0,0 +1,23 @@
+//Chocola Problem
+
+//We are given bar of chocolate composed of mxn square pieces.One should break the chocolate into single squares.Each
+//break of a part of the chocolate is charged a cost expressed by a positive integer. This cost does not depend on the 
+//size of the part that is being broken but only depends on the line the break goes along.Let us denote the costs of
+//breaking along consecutive vertical lines with x1,x2, ...,xm-1 and along horizontal lines with y1,y2, ...,yn-1.
+//Compute the minimal cost of breaking the whole chocolate into single squares
+//               |      |
+//               |      |
+//               |      |
+//    y1  --------------------------    
+//               |      |
+//               |      |
+//    y2  --------------------------
+//               |      |
+//               |      |
+//               |      |
+//               x1     x2
+
+
+public class H_Chocola {
+    
+}
