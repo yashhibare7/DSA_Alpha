d090b3a2fa1195174b75d14661161f1384d17bd2
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-25 23:45:47 +0530

    Count Of Nodes Of Tree Solution

---
+++ 29_BinaryTrees/H_CountOfNodesOfTree.java
@@ -0,0 +1,44 @@
+//Count Of Nodes Of a Tree
+
+/*
+          1
+        /  \
+       2   3
+     /  \   \
+    4   5    6
+*/
+
+public class H_CountOfNodesOfTree {
+    static class Node {
+        int data;
+        Node left , right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static int count(Node root){
+        if(root==null){
+            return 0;
+        }
+        int lcount = count(root.left);
+        int rcount = count(root.right);
+        
+        int treeCount = lcount + rcount+1;
+
+        return treeCount;
+    }
+    public static void main(String args[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(count(root));
+    }
+}
