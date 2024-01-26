d21208a3634e4f6e5df6a7d1d397ac2697e036f8
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-26 23:21:28 +0530

    Subtree Of Another Tree Problem Statement

---
+++ 30_BinaryTrees_(Part-2)/C_SubtreeOfAnotherTree.java
@@ -0,0 +1,17 @@
+//Subtree of another Tree
+
+/*
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure
+and node values of subRoot and false otherwise.
+
+            tree                        subTree
+             1                             2
+          /     \                        /   \
+        2        3                      4     5
+      /  \        \
+     4    5        6
+*/
+
+public class C_SubtreeOfAnotherTree {
+    public static void main(String args){}
+}
