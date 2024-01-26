c1197f64065254359b990b249429225071d235dc
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-27 01:02:52 +0530

    init

--- 30_BinaryTrees_(Part-2)/C_SubtreeOfAnotherTree.java
+++ 30_BinaryTrees_(Part-2)/C_SubtreeOfAnotherTree.java
@@ -13,5 +13,49 @@ and node values of subRoot and false otherwise.
 */
 
 public class C_SubtreeOfAnotherTree {
-    public static void main(String args){}
+    static class Node{
+        int data;
+        Node left, right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot){
+        if (subRoot == null){
+            return true;
+        }
+    }
+    public static void main(String args){
+        /*
+         tree                       
+             1                           
+          /     \                        
+        2        3                      
+      /  \        \
+     4    5        6
+         */
+
+         Node root = new Node(1);
+         root.left = new Node(2);
+         root.right = new Node(3);
+         root.left.left = new Node(4);
+         root.left.right = new Node(5);
+         root.right.right = new Node(6);
+
+         /*
+           subTree
+               2
+             /   \
+            4     5                                  
+          */
+
+          Node subRoot = new Node(2);
+          subRoot.left = new Node(4);
+          subRoot.right = new Node(5);
+
+    }
 }
