a96ea79962b380afcaff28776fa1a553bc02d361
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-27 02:37:02 +0530

    Top View Of Tree Problem Statement

---
+++ 30_BinaryTrees_(Part-2)/D_TopViewOfTree.java
@@ -0,0 +1,26 @@
+//Top View Of Tree
+
+/*                       
+             1                           
+          /     \                        
+        2        3                      
+      /  \     /  \
+     4    5  4     6
+
+     Ans:- 4, 2, 1, 3, 6
+         */
+
+public class D_TopViewOfTree {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void main(String args[]){
+    }
+}
