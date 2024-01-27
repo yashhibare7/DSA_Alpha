a80e52c706d1cff9aa044ef7e1c72e3005d1f4c2
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-27 19:20:19 +0530

    Kth Level Of a tree problem statement

---
+++ 31_BinaryTrees_(Part-3)/KthLevelOfTree.java
@@ -0,0 +1,36 @@
+//Kth Level Of a Tree
+
+/*
+                   1
+                /    \
+              2       3
+            /  \     / \
+          4     5   6   7
+
+          K = 3
+          output : 4,5,6,7
+*/
+
+
+import java.util.*;
+public class KthLevelOfTree {
+    static class Node{
+        int data;
+        Node left,right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void main(String args[]){
+        /*
+                   1
+                /    \
+              2       3
+            /  \     / \
+          4     5   6   7
+         */
+    }
+}
