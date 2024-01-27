dbf0b892429ea2bd7655c8ea4e52e88bbd0c1d40
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-27 19:55:54 +0530

    Kth Level Of Tree Solution

--- 31_BinaryTrees_(Part-3)/KthLevelOfTree.java
+++ 31_BinaryTrees_(Part-3)/KthLevelOfTree.java
@@ -24,6 +24,20 @@ public class KthLevelOfTree {
             this.right = null;
         }
     }
+
+    public static void KLevel(Node root, int level, int k){
+      if (root == null) {
+        return;
+      }
+
+      if (level == k) {
+        System.out.print(root.data+" ");
+        return;
+      }
+
+      KLevel(root.left, level+1, k);
+      KLevel(root.right, level+1, k);
+    }
     public static void main(String args[]){
         /*
                    1
@@ -32,5 +46,15 @@ public class KthLevelOfTree {
             /  \     / \
           4     5   6   7
          */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int k = 2;
+        KLevel(root, 1, k);
     }
 }
