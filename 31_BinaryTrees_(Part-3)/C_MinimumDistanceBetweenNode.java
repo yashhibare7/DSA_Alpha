0bc8087e87d73d103a80e3248fd877cb5be27d66
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-29 19:56:39 +0530

    Minimum Distance Between Node Problem Statement

---
+++ 31_BinaryTrees_(Part-3)/C_MinimumDistanceBetweenNode.java
@@ -0,0 +1,13 @@
+/*
+                   1
+                /    \
+              2       3
+            /  \     / \
+          4     5   6   7
+          n1 = 4, n2 = 6
+          dist = 4
+         */
+
+public class C_MinimumDistanceBetweenNode {
+    
+}
