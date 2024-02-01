89b3cee16dacaf764cd1c27709f234503b5bcf93
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-02-01 18:03:53 +0530

    Search a BST Problem Statement

---
+++ 32_Binary_Search_Tree(Part1)/C_SearchABST.java
@@ -0,0 +1,16 @@
+/*
+ 
+
+                    8
+                  /   \
+                 5     10                  key = 1
+               /  \      \
+             3     6      11
+           /  \             \
+          1    4             14
+ */
+
+
+public class C_SearchABST {
+    public static void main(String args[]){}
+}
