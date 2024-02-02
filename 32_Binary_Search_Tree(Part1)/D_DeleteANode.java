728c3f21457fc566d41b84f8f58248f6c283ad60
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-02-02 23:38:26 +0530

    Delete a Node Solution

--- 32_Binary_Search_Tree(Part1)/D_DeleteANode.java
+++ 32_Binary_Search_Tree(Part1)/D_DeleteANode.java
@@ -46,6 +46,8 @@
             1    4             14 
  */
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class D_DeleteANode {
     static class Node{
         int data;
@@ -78,8 +80,56 @@ public class D_DeleteANode {
         System.out.print(root.data+" ");
         inorder(root.right);
     }
+
+    public static Node delete(Node root, int val){
+      if (root.data < val) {
+        root.right = delete(root.right, val);
+      } else if (root.data > val) {
+        root.left = delete(root.left, val);
+      } else{//voila
+        //case 1 - leaf Node
+        if (root.left == null && root.right == null) {
+          return null;
+        }
+
+        //case 2 - single child
+        if (root.left == null) {
+          return root.right;
+        } else if (root.right == null) {
+          return root.left;
+        }
+
+        //case 3 - both children
+        Node IS = findInorderSuccessor(root.right);
+        root.data = IS.data;
+        root.right = delete(root.right, IS.data);
+      }
+
+      return root;
+    }
+
+    public static Node findInorderSuccessor(Node root){
+      while (root.left != null) {
+        root = root.left;
+      }
+      return root;
+    }
+
+
     public static void main(String args[]){
-        int values[] = {5,1,3,4,2,7};
+        int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
+
+        for(int i=0;i<values.length;i++){
+          root = insert(root, values[i]);
+      }
+
+      inorder(root);
+      System.out.println();
+
+      root = delete(root, 1);
+      System.out.println();
+
+      inorder(root);
     }
 }
