b7bcd82d67be34d8de6ab2fd4ab1265b3e50740d
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-02-14 20:53:51 +0530

    PQ In JCF Solution

--- 34_Heaps/B_PQInJCF.java
+++ 34_Heaps/B_PQInJCF.java
@@ -1,4 +1,17 @@
+import java.util.PriorityQueue;
+
 public class B_PQInJCF{
-    
-    public static void main(String args[]){}
+    public static void main(String args[]){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();  
+
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+    }
 }
