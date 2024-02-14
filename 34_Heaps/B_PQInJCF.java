be17e0f0df7d2a4eb90e95e280ad57ddd6d95735
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-02-14 21:11:28 +0530

    init

--- 34_Heaps/B_PQInJCF.java
+++ 34_Heaps/B_PQInJCF.java
@@ -1,8 +1,9 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class B_PQInJCF{
     public static void main(String args[]){
-        PriorityQueue<Integer> pq = new PriorityQueue<>();  
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());  
 
         pq.add(3);
         pq.add(4);
