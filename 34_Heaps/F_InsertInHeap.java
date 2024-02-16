63969bf77c4a1f91675c24c5ce19ce4a1c45dce6
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-02-17 02:35:15 +0530

    Insert In Heap

---
+++ 34_Heaps/F_InsertInHeap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class F_InsertInHeap{
+    static class Heap{
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data){
+            //add at last idx
+            arr.add(data);
+
+            int x = arr.size()-1; //x is child index
+            int par = (x-1)/2; //par index
+
+            while (arr.get(x) < arr.get(par)) {
+                //swap
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par,temp);
+            }
+        }
+    }
+    public static void main(String args[]){}
+}
