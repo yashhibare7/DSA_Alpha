ea0215deead916b8f4933bad33369ae4b55ed974
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-02-17 20:34:52 +0530

    Peek From Heaps Solution

--- 34_Heaps/G_PeekFromHeaps.java
+++ 34_Heaps/G_PeekFromHeaps.java
@@ -17,6 +17,9 @@ public class G_PeekFromHeaps {
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par,temp);
+
+                x = par;
+                par = (x-1)/2;
             }
         }
 
@@ -24,17 +27,59 @@ public class G_PeekFromHeaps {
             return arr.get(0);
         }
 
+        private void heapify(int i){
+            int left = 2*i+1;
+            int right = 2*i+2;
+            int minIdx = i;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != i) {
+                //swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+        }
+
         public int remove(){
             int data = arr.get(0);
 
             //swap 1 - swap first & last
             int temp = arr.get(0);
-            arr.set(0,arr.size()-1);
+            arr.set(0,arr.get(arr.size()-1));
             arr.set(arr.size()-1,temp);
 
             //swap 2 - delete last
             arr.remove(arr.size()-1);
+
+            //swap 3 - heapify
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
         }
     }
-    public static void main(String args[]){}
+    public static void main(String args[]){
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+
+        while (!h.isEmpty()) {
+            System.out.println(h.peek());
+            h.remove();
+        }
+    }
 }
