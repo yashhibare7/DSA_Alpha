647f182012a3bbea78a7163a08a10919e1c2ff75
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-03-05 23:08:12 +0530

    Nearby Cars Problem Statement

---
+++ 34_Heaps/J_NearbyCars.java
@@ -0,0 +1,14 @@
+/*
+ Nearby Cars
+  We are given N points in a 2D plane which are locations of N cars.
+  If we are at the origin, print the nearest K cars.
+
+  C0 (3,3)    K = 2
+  C1 (5,-1)
+  C2 (-2,4)
+
+  ans = C0 & C2
+ */
+public class J_NearbyCars {
+    public static void main(String args[]){}
+}
