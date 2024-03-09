a3d239f2641097c755774ca65d99888cea3d992d
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-03-10 01:21:55 +0530

    Weakest Soldier Question

---
+++ 34_Heaps/L_WeakestSoldier.java
@@ -0,0 +1,21 @@
+/*
+ Weakest Soldier
+
+ we are given an mxn binary matrix of 1's (soldiers) and 0's (civilians). The soldiers are positioned in front of the
+ civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+ A row i is weaker than a row j if one of the following is true:
+   - The number of soldiers in row i is less than the number of soldiers in row j.
+   - Both rows have the same number of soldiers and i<j.
+ Find the k weakest rows.
+
+      m=4,n=4,k=2      ans = row0 & row2
+      1 0 0 0
+      1 1 1 1
+      1 0 0 0
+      1 0 0 0
+ */
+public class L_WeakestSoldier {
+    public static void main(String args[]) {
+    }
+}
