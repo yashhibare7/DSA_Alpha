fe5b43f609a0e160b3116309e91788f2f1a15dcb
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-03-10 23:25:30 +0530

    init

--- 34_Heaps/L_WeakestSoldier.java
+++ 34_Heaps/L_WeakestSoldier.java
@@ -16,6 +16,26 @@
       1 0 0 0
  */
 public class L_WeakestSoldier {
+  static class Row implements Comparable<Row> {
+    int soldiers;
+    int idx;
+
+    public Row(int soldiers,int idx){
+      this.soldiers = soldiers;
+      this.idx = idx;
+    }
+
+    @Override
+    public int compareTo(Row r2){
+      
+    }
+  }
     public static void main(String args[]) {
+      int army[][] = {{1, 0, 0, 0},
+                      {1, 1, 1, 1},
+                      {1, 0, 0, 0},
+                      {1, 0, 0, 0}};
+
+      int k = 2;
     }
 }
