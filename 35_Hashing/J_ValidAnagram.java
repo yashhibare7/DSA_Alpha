d9a55f6a96027e0450b1bf9ed7b8868e7bd77725
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-04-21 23:55:45 +0530

    Valid Anagram

---
+++ 35_Hashing/J_ValidAnagram.java
@@ -0,0 +1,47 @@
+/*
+ Valid Anagram
+
+ Given two string s and t, return true if t ia an anagram of s, and false otherwise.
+ An Anagram is a word or phrase formed by rearranging the letters of a dpfferent word or phrase, typically using all the original letters exactly once.
+
+ s = "race"   t = "care"   TRUE
+ s = "heart"  t = "earth"  TRUE
+ s = "tulip"  t = "lipid"  FALSE
+ */
+
+import java.util.*;
+
+public class J_ValidAnagram {
+    public static boolean isAnagram(String s,String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+
+        for(int i=0;i<t.length();i++){
+            char ch = t.charAt(i);
+            if(map.get(ch)!=null){
+                if(map.get(ch)==1){
+                    map.remove(ch);
+                }else{
+                    map.put(ch, map.get(ch)-1);
+                }
+            }else{
+                return false;
+            }
+        }
+
+        return map.isEmpty();
+    }
+    public static void main(String args[]){
+        String s="race";
+        String t="care";
+
+        System.out.println(isAnagram(s, t));
+    }
+}
