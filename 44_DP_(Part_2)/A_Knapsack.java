d7f23dde97b0d4784de2c3ccb4736d029099abc7
yhibare <96299060+yashhibare7@users.noreply.github.com>
2025-08-17 20:45:06 +0530

    0-1 knapsack and Unbounded Kanpsack intro

---
+++ 44_DP_(Part_2)/A_Knapsack.java
@@ -0,0 +1,52 @@
+/*
+ 0 -1 Knapsack
+
+ Types of knapsack problems
+
+ 1. Fractional knapsack (Greedy)
+ 2. 0-1 kanpsack
+ 3. Unbounded Kanpsack
+
+ 0-1 knapsack - 
+
+    eg. 
+             val  weight
+    fridge - 100  10
+    laptop - 50   5       
+    phone  - 30   2                      
+    tablet - 25   1
+    pencil - 5    0.5
+    book   - 15   1
+
+    -------------
+    |           |
+    |           |  ---- capacity 10
+    |           |
+    ------------
+
+    Case 1 - fridge full weight=10 val=100
+    Case 2 - laptop,Phone, tablet,pencil,book weight=9.5 val=135
+
+ Unbounded Kanpsack (items repeat) - 
+
+    eg. 
+             val  weight
+    fridge - 100  10
+    laptop - 50   5       
+    phone  - 30   2                      
+    tablet - 25   1
+    pencil - 5    0.5
+    book   - 15   1
+
+    -------------
+    |           |
+    |           |  ---- capacity 10
+    |           |
+    ------------
+
+    Case 1 - 10 tablets 25 x 10 = 250
+ */
+
+public class A_Knapsack {
+
+}
