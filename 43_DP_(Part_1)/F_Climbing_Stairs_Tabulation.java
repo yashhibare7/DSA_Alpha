25c63da15fe8c2869c8bd2101008f862ebfd2f0f
yhibare <96299060+yashhibare7@users.noreply.github.com>
2025-08-17 17:29:58 +0530

    Climbing Stairs problem (Tabulation)

--- 43_DP_(Part_1)/F_Climbing_Stairs_Tabulation.java
+++ 43_DP_(Part_1)/F_Climbing_Stairs_Tabulation.java
@@ -1,5 +1,20 @@
 public class F_Climbing_Stairs_Tabulation {
+    public static int countWayTab(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                dp[i] = dp[i-1]+0;
+            } else {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
     public static void main(String args[]){
         int n = 5;
+
+        System.out.println(countWayTab(n));
     }
 }
