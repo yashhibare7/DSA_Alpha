4a537c103e977e987348c7380830cb6b7c498fe8
yhibare <96299060+yashhibare7@users.noreply.github.com>
2025-09-07 20:13:19 +0530

    Knapsack Tabulation

--- 44_DP_(Part_2)/D_0_1_Knapsack_tabulation.java
+++ 44_DP_(Part_2)/D_0_1_Knapsack_tabulation.java
@@ -1,4 +1,13 @@
 public class D_0_1_Knapsack_tabulation {
+    public static void print(int dp[][]){
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     public static int knapsackTab(int val[], int wt[], int W){
         int n = val.length;
         int dp[][] = new int[n+1][W+1];
@@ -24,7 +33,7 @@ public class D_0_1_Knapsack_tabulation {
                 }
             }
         }
-
+        print(dp);
         return dp[n][W];
     }
     public static void main(String args[]){
